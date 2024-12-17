6ec8300cb2696bd835e4cbef99aad4593e03afe0
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-12-17 16:24:30 +0330

    finish codding team page

---
+++ src/Pages/Team/Team.jsx
@@ -0,0 +1,66 @@
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import TeamBox from "../../Components/TeamBox/TeamBox";
+import PageHeader from "../../Components/PageHeader/PageHeader";
+import BreadCrumb from "../../Components/Breadcrumb/BreadCrumb";
+
+export default function Team() {
+    return (
+
+        <>
+            <Header changeMode={true}/>
+            <main>
+                <PageHeader title='اعضای تیم'/>
+                <BreadCrumb page='تیم'/>
+                <section className='mt-14 mb-[60px] md:mb-[100px]'>
+                    <div className="container">
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
+                            <TeamBox
+                                img='/images/team/03%20(1).jpg'
+                                name='رونی جفرا'
+                                job='توسعه دهنده'
+                            />
+                            <TeamBox
+                                img='/images/team/02%20(1).jpg'
+                                name='میکایل'
+                                job='کارگردان'
+                            />
+                            <TeamBox
+                                img='/images/team/01%20(1).jpg'
+                                name='موسی ماری'
+                                job='مدیریت'
+                            />
+                            <TeamBox
+                                img='/images/team/05.jpg'
+                                name='کالوین لورس'
+                                job='امنیت'
+                            />
+                            <TeamBox
+                                img='/images/team/04.jpg'
+                                name='جعفر عباسی'
+                                job='طراح'
+                            />
+                            <TeamBox
+                                img='/images/team/06.jpg'
+                                name='لوسی'
+                                job='سئو'
+                            />
+                            <TeamBox
+                                img='/images/team/08.jpg'
+                                name='صدرا خان'
+                                job='ادیتور'
+                            />
+                            <TeamBox
+                                img='/images/team/07.jpg'
+                                name='موسی ماری'
+                                job='مدیریت'
+                            />
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <Footer/>
+        </>
+
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Index from "./Pages/Index/Index";
 import AboutUs from "./Pages/AboutUs/AboutUs";
 import Services from "./Pages/Services/Services";
 import History from "./Pages/History/History";
+import Team from "./Pages/Team/Team";
 
 
 let routes = [
@@ -9,6 +10,7 @@ let routes = [
     { path: '/aboutus', element: <AboutUs/> },
     { path: '/services', element: <Services/> },
     { path: '/history', element: <History/> },
+    { path: '/team', element: <Team/> },
 ]
 
 export default routes
