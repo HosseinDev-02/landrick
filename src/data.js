60993740756e77ebd2322c8181a01f25bbe12348
HosseinDev-02 <rostamidev2002@gmail.com>
2025-10-06 11:17:19 +0330

    add data.js file

---
+++ src/data.js
@@ -0,0 +1,115 @@
+const menuItems = [
+    {
+        id: 1,
+        title: "صفحه اصلی",
+        href: "/",
+    },
+    {
+        id: 2,
+        title: "وبلاگ",
+        href: "/blogs",
+    },
+    {
+        id: 3,
+        title: "شرکت",
+        href: "#",
+        subLinks: [
+            {
+                id: 1,
+                title: "خدمات",
+                href: "/services",
+            },
+            {
+                id: 2,
+                title: "تاریخچه",
+                href: "/history",
+            },
+            {
+                id: 3,
+                title: "تیم ما",
+                href: "/team",
+            },
+            {
+                id: 4,
+                title: "قیمت گذاری",
+                href: "/pricing",
+            },
+            {
+                id: 5,
+                title: "درباره ما",
+                href: "/aboutus",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "تماس با ما",
+        href: "/contact-us",
+    },
+];
+const footerComponyLinks = [
+    {
+        id: "1",
+        title: "درباره ما",
+        href: "/aboutus",
+    },
+    {
+        id: "2",
+        title: "خدمات",
+        href: "/services",
+    },
+    {
+        id: "3",
+        title: "تیم",
+        href: "/team",
+    },
+    {
+        id: "4",
+        title: "قیمت گذاری",
+        href: "/pricing",
+    },
+    {
+        id: "5",
+        title: "پروژه",
+        href: "#",
+    },
+    {
+        id: "6",
+        title: "وبلاگ",
+        href: "/blogs",
+    },
+    {
+        id: "7",
+        title: "مشاغل",
+        href: "#",
+    },
+];
+const useFulLinks = [
+    {
+        id: '1',
+        title: 'خدمات سایت',
+        href: '#'
+    },
+    {
+        id: '2',
+        title: 'حریم خصوصی',
+        href: '#'
+    },
+    {
+        id: '3',
+        title: 'مستند',
+        href: '#'
+    },
+    {
+        id: '4',
+        title: 'تغییرات',
+        href: '#'
+    },
+    {
+        id: '5',
+        title: 'اجزاء',
+        href: '#'
+    },
+]
+
+export { menuItems, footerComponyLinks, useFulLinks };
